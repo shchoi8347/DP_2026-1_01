75f358654e447374c7cc333ce1e400bd58045c89
shchoi8347 <csh@duksung.ac.kr>
2026-03-17 10:17:01 +0900

    3월17일(화) 수업 종료

---
+++ src/practice/ch02/inheritance/Banner.java
@@ -0,0 +1,18 @@
+package practice.ch02.inheritance;
+
+// 이미 존재하는 클래스, Vendor 클래스, 강아지(멍멍() 제공), 수정할 수 없음
+public class Banner {
+    private String string;
+
+    public Banner(String string) {
+        this.string = string;
+    }
+
+    public void showWithParen() {
+        System.out.println("(" + string + ")");
+    }
+
+    public void showWithAster() {
+        System.out.println("*" + string + "*");
+    }
+}
---
+++ src/practice/ch02/inheritance/PrintBannerAdater01.java
@@ -0,0 +1,16 @@
+package practice.ch02.inheritance;
+
+// 클라이언트가 원하는 인터페이스를 구현한 어댑터 클래스
+public class PrintBannerAdater01 extends Banner implements Print {
+
+    @Override
+    public void printWeak() {
+        showWithParen();
+    }
+
+    @Override
+    public void printStrong() {
+
+    }
+
+}
