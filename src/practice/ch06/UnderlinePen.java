52e4c06171a6e37ecd8ee09f8d36cf130086399a
shchoi8347 <csh@duksung.ac.kr>
2026-04-07 10:17:21 +0900

    4/7 수업종료

---
+++ src/practice/ch06/UnderlinePen.java
@@ -0,0 +1,35 @@
+package practice.ch06;
+
+import practice.ch06.framework.Product;
+
+public class UnderlinePen implements Product {
+    private char ulchar;
+
+    public UnderlinePen(char ulchar) {
+        this.ulchar = ulchar;
+    }
+
+    @Override
+    public void use(String s) {
+        int ulen = s.length();
+
+        System.out.println(s);
+
+        for (int i = 0; i < ulen; i++) {
+            System.out.print(ulchar);
+        }
+        System.out.println();
+    }
+
+    @Override
+    // 복제 메서드
+    public Product createCopy() {
+        Product p = null;
+        try {
+            p = (Product) clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return p;
+    }
+}
